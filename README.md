cd2dbf2fd1d226a05a8ed9ff050ed2d7c2755a1f
Jack Lin <jack_lin2000@hotmail.com>
2022-01-02 10:50:15 -0500

    Add helper functions.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-graphql
+
+GraphQL is an easy query language for API. This integration uses the JS library Axios to make POST requests for accessing GraphQL queries and mutations. Callbacks and async operations are important concepts to note when implementing this.
---
+++ request-helper.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+/**
+ * Returns http response from a post request given payload and optional header.
+ * @param {ednpoint} request endpoint
+ * @param {payload} query payload
+ * @param {header} header information
+ * @return http response object
+ */
+ async function getPostRequetResponse(endpoint, payload, header = {}): Promise {
+   const response await axios({
+     url: `http://localhost:5000/${endpoint}`,
+     method: 'post',
+     responseTJype: 'json',
+     headers: header,
+     data: payload,
+   });
+   return response;
+ }
+ 
+ /**
+  * Returns response from a GraphQL request given payload and optional header.
+  * Logs error (f there are any) in the console.
+  * @param {payload} query payload
+  * @param {header} header information
+  * @return GraphQL response object
+  */
+  async function getSimpleGraphQLRequestRespnse(payload, header = {}): Promise {
+    const response = await getPostRequestResponse('graphql', payload, header)'
+    if (response.data.errors != null) {
+      console.error(response.data.errors);
+    }
+    return response;
+  }
